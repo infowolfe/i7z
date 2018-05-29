863138db11ee137b8e6afa612d6013b6069d0111
Nicolas Richart <nicolas.richart@epfl.ch>
2018-05-29 16:18:01 +0200

    Making the print offset relative to the number of cores per sockets

--- src/i7z_Dual_Socket.c
+++ src/i7z_Dual_Socket.c
@@ -234,14 +234,15 @@ void print_i7z_socket(struct cpu_socket_info socket_0, int printw_offset, int PL
 
         if (socket_0.socket_num == 0)
         {
-            mvprintw (31, 0, "C0 = Processor running without halting");
-            mvprintw (32, 0, "C1 = Processor running with halts (States >C0 are power saver)");
-            mvprintw (33, 0, "C3 = Cores running with PLL turned off and core cache turned off");
-            mvprintw (34, 0, "C6 = Everything in C3 + core state saved to last level cache");
-            mvprintw (35, 0, "  Above values in table are in percentage over the last 1 sec");
-            mvprintw (36, 0, "[core-id] refers to core-id number in /proc/cpuinfo");
-            mvprintw (37, 0, "'Garbage Values' message printed when garbage values are read");
-            mvprintw (38, 0, "  Ctrl+C to exit");
+            int offset = (numCores + 7) * 2 + 4;
+            mvprintw (0 + offset, 0, "C0 = Processor running without halting");
+            mvprintw (1 + offset, 0, "C1 = Processor running with halts (States >C0 are power saver)");
+            mvprintw (2 + offset, 0, "C3 = Cores running with PLL turned off and core cache turned off");
+            mvprintw (3 + offset, 0, "C6 = Everything in C3 + core state saved to last level cache");
+            mvprintw (4 + offset, 0, "  Above values in table are in percentage over the last 1 sec");
+            mvprintw (5 + offset, 0, "[core-id] refers to core-id number in /proc/cpuinfo");
+            mvprintw (6 + offset, 0, "'Garbage Values' message printed when garbage values are read");
+            mvprintw (7 + offset, 0, "  Ctrl+C to exit");
         }
 
         numCores = core_list_size_phy;
@@ -753,7 +754,7 @@ void print_i7z ()
                   C3_time[socket_num], C6_time[socket_num], tvstart[socket_num], tvstop[socket_num], &max_observed_cpu_socket2);
         }else{*/
         socket_num=1;
-        printw_offset=14;
+        printw_offset=socket_0.num_physical_cores + 7;
         print_i7z_socket(socket_1, printw_offset, PLATFORM_INFO_MSR,  PLATFORM_INFO_MSR_high, PLATFORM_INFO_MSR_low,
                          online_cpus, cpu_freq_cpuinfo, one_second_sleep, TURBO_MODE, HT_ON_str, &kk_2,
                          old_val_CORE[socket_num], old_val_REF[socket_num], old_val_C3[socket_num], old_val_C6[socket_num], old_val_C7[socket_num],
